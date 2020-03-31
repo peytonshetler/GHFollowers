a291c97b953770112a908bedc4d20854f75716ae
Peyton Shetler <peyton.shetler@gmail.com>
2020-03-31 15:58:04 -0500

    Completed the "Get Started" section of the course!

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Peyton Shetler on 3/31/20.
+//  Copyright © 2020 Peyton Shetler. All rights reserved.
+//
+// MARK: Notes on System Color: The reason we're using these here instead of regular .blue/.pink, is because they will automatically adjust to light & dark mode.
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
 //  Created by Peyton Shetler on 3/31/20.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
     }
-
-
 }
-
